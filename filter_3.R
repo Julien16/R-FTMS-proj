cc3d9fcbad82b8e1297df4b78ce55c3d76f166b8
J <Julien16@users.noreply.github.com>
2016-01-21 16:02:54 +0900

    Update filter_3.R

--- filter_3.R
+++ filter_3.R
@@ -1,5 +1,5 @@
-#' Zahler 3
-zahler_3 <- function(data, AI_max = NULL, AI_min = NULL) {
+#' filter_3
+filter_3 <- function(data, AI_max = NULL, AI_min = NULL) {
   
   if(is.null(AI_max)){
     stop("You should set AI_max")
@@ -10,12 +10,12 @@ zahler_3 <- function(data, AI_max = NULL, AI_min = NULL) {
   }
   
   attach(data)
-  zahler3 <- ifelse((C-O-N)==0, 1,
+  filter3 <- ifelse((C-O-N)==0, 1,
                   ifelse(((1+C-O-0.5*HIon)/(C-O-N))<AI_max,
                          ifelse(((1+C-O-0.5*HIon)/(C-O-N))>AI_min,
                                 ifelse(N/C<=NtoC_max,1
                                        ,0),0),0))
   detach(data)
   
-  return(zahler3)
+  return(filter3)
 }
