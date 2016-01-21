b8c352e98656f6ff536df9adfbeb4ff176a6819e
J <Julien16@users.noreply.github.com>
2016-01-21 16:01:16 +0900

    Update filter_2.R

--- filter_2.R
+++ filter_2.R
@@ -1,5 +1,5 @@
-#' zahler_2 function to compute
-zahler_2 <- function(data, DBEtoC_min = NULL, DBEtoC_max = NULL, OplusNtoC_max = NULL) {
+#' filter_2 function to compute
+filter_2 <- function(data, DBEtoC_min = NULL, DBEtoC_max = NULL, OplusNtoC_max = NULL) {
   if(is.null(DBEtoC_min)){
     stop("You should insert DBEtoC_min")
   }
@@ -13,12 +13,12 @@ zahler_2 <- function(data, DBEtoC_min = NULL, DBEtoC_max = NULL, OplusNtoC_max =
   }
   
   attach(data)
-  zahler2 <- ifelse((1+0.5*(2*C-HIon+N))/C<DBEtoC_max,
+  filter2 <- ifelse((1+0.5*(2*C-HIon+N))/C<DBEtoC_max,
                     ifelse((1+0.5*(2*C-HIon+N))/C>DBEtoC_min,
                            ifelse((O+N)/C<OplusNtoC_max,
                                   ifelse(C>5,1
                                          ,0),0),0),0)
   detach(data)
   
-  return(zahler2)
+  return(filter2)
 }
