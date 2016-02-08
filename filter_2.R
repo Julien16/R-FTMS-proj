fa7312e7de09d5e3b7865233b6c4c5a5f8c9a55f
Julien16 <Julien16@users.noreply.github.com>
2016-02-08 16:42:52 +0900

    filter_2 & filter_3
    just make it more consistent

--- filter_2.R
+++ filter_2.R
@@ -1,15 +1,15 @@
-#' filter_2 function to compute
+#' Compute filter_2 values
 filter_2 <- function(data, DBEtoC_min = NULL, DBEtoC_max = NULL, OplusNtoC_max = NULL) {
   if(is.null(DBEtoC_min)){
-    stop("You should insert DBEtoC_min")
+    stop("You should insert lowest DBE/C value: DBEtoC_min")
   }
   
   if(is.null(DBEtoC_max)){
-    stop("You should insert DBEtoC_max")
+    stop("You should insert highest DBE/C value: DBEtoC_max")
   }
   
   if(is.null(OplusNtoC_max)){
-    stop("You should insert OplusNtoC_max")
+    stop("You should insert highest (O+N)/C value: OplusNtoC_max")
   }
   
   attach(data)
--- filter_3.R
+++ filter_3.R
@@ -1,12 +1,12 @@
-#' filter_3
+#' Compute filter_3 values
 filter_3 <- function(data, AI_max = NULL, AI_min = NULL) {
   
   if(is.null(AI_max)){
-    stop("You should set AI_max")
+    stop("You should insert highest AI value: AI_max")
   }
   
   if(is.null(AI_min)){
-    stop("You should set AI_min")
+    stop("You should insert lowest AI value: AI_min")
   }
   
   attach(data)
