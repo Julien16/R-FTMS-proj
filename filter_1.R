cc8ebd81c69cf0c58ae50cc02a5f025303e6b0ad
Julien16 <Julien16@users.noreply.github.com>
2016-02-08 16:42:04 +0900

    filter_1 keep it flex!
    The role of the function is to filter data. different people want to
    apply different filtration ;o)

--- filter_1.R
+++ filter_1.R
@@ -1,24 +1,48 @@
 #' Compute filter_1 values
 
-filter_1 <- function(data,OtoC_min = 0, OtoC_max = 1, Intensity_Min=0, 
-                     Intensity_Max = 235000000, ExpMass_Min = 149, 
-                     ExpMass_Max = 800, OC = NULL, 
+filter_1 <- function(data, OtoC_min = NULL, OtoC_max = NULL, Intensity_Min = NULL, 
+                     Intensity_Max = NULL, ExpMass_Min = NULL, 
+                     ExpMass_Max = NULL, OC = NULL, 
                      Intensity = NULL) {
   
+  if(is.null(OtoC_min)){
+    stop("You should insert lowest O/C ratio threshold value: OtoC_min")
+  }
+  
+  if(is.null(OtoC_max)){
+    stop("You should insert highest O/C ratio threshold value: OtoC_max")
+  }
+  
+  if(is.null(Intensity_Min)){
+    stop("You should insert lowest intensity threshold value: Intensity_Min")
+  }
+  
+  if(is.null(Intensity_Max)){
+    stop("You should insert highest intensity threshold value: Intensity_Max")
+  }
+  
+  if(is.null(ExpMass_Min)){
+    stop("You should insert lowest mass threshold value: ExpMass_Min")
+  }
+  
+  if(is.null(ExpMass_Max)){
+    stop("You should insert highest mass threshold value: ExpMass_Max")
+  }
+  
   if(is.null(Intensity)){
-    stop("You should insert Intensity data")
+    stop("You should insert Intensity value: Intensity")
   }
   
   if(is.null(OC)){
-    stop("You should insert Oxygen/Carbon data")
+    stop("You should insert Oxygen/Carbon value: OC")
   }
   
   filter1 <- ifelse(OC<OtoC_max,
                     ifelse(OC>OtoC_min,
-                           ifelse(formcalc_raw$Intensity<Intensity_Max,
-                                  ifelse(formcalc_raw$Intensity>Intensity_Min,
-                                         ifelse(formcalc_raw$ExpMass<ExpMass_Max,
-                                                ifelse(formcalc_raw$ExpMass>ExpMass_Min,1
+                           ifelse(formula_list$Intensity<Intensity_Max,
+                                  ifelse(formula_list$Intensity>Intensity_Min,
+                                         ifelse(formula_list$ExpMass<ExpMass_Max,
+                                                ifelse(formula_list$ExpMass>ExpMass_Min,1
                                                        ,0),0),0),0),0),0)
   
   return (filter1)
