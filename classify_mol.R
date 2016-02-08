87d6468a7ea3d86623c569ec579bd8976376475d
Julien16 <Julien16@users.noreply.github.com>
2016-02-08 16:39:42 +0900

    classify_mol.R From 6 to 4 classes
    turn CHON2 into CHON and CHON2S into CHONS. Let's keep it simple first.
    Then we have to think about how we could keep this function flexible,
    for those having P element or you want to differentiate CHON2 and CHON
    or CHON2S and CHONS... just examples, but other possibilities

--- classify_mol.R
+++ classify_mol.R
@@ -1,18 +1,15 @@
-#' Function to classify molecules from raw FTMS data
+#' Function to classify molecules from raw FTMS formulas data
 
 classify_mol <- function(data) {
   attach(data)
   mol_type <- ifelse(C>=1 & HIon>=1 & O>=1 & N==0 & S==0, "CHO",
-                     ifelse(C>=1 & HIon>=1 & O>=1 & N==1 & S==0, "CHON",
+                     ifelse(C>=1 & HIon>=1 & O>=1 & N>=1 & S==0, "CHON",
                             ifelse(C>=1 & HIon>=1 & O>=1 & N==0 & S==1, "CHOS",
-                                   ifelse(C>=1 & HIon>=1 & O>=1 & N==1 & S==1, "CHONS",
-                                          ifelse(C>=1 & HIon>=1 & O>=1 & N==2 & S==0, "CHON2",
-                                                 ifelse(C>=1 & HIon>=1 & O>=1 & N==2 & S==1, "CHON2S", NA)
-                                          )
+                                   ifelse(C>=1 & HIon>=1 & O>=1 & N>=1 & S==1, "CHONS", NA)
                                    )
                             )
                      )
-  )
   detach(data)
   return(mol_type)
 }
+
